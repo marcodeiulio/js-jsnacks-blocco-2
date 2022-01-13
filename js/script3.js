75f44258dc63d0fefd1bff3b6a3af9d291bbf5b3
marcodeiulio <mdeiulio@icloud.com>
2022-01-13 16:20:59 +0100

    - Completed snack3

---
+++ js/script3.js
@@ -0,0 +1,16 @@
+/* Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati. */
+
+const names = ['Marco', 'Angela', 'Chiara', 'Giambabolo'];
+const surnames = ['de Iulio', 'Isabella', 'Casadei', 'Arcistecco'];
+
+let fakeList = [];
+
+while (fakeList.length < 3) {
+	randomName = names[Math.floor((Math.random() * names.length))];
+
+	randomSurname = surnames[Math.floor((Math.random() * surnames.length))];
+
+	fakeList.push('Invitato: ' + randomName + ' ' + randomSurname);
+
+}
+console.log(fakeList);
